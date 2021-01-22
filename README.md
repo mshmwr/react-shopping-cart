8976b9dbec2c8c3e98ba17fabe24f74ea55e0a99
mshmwr <mshmwr20@gmail.com>
2021-01-22 23:19:54 +0800

    checkout form

--- README.md
+++ README.md
@@ -17,9 +17,24 @@
     <option value="">ALL</option>
   </select>
   ```
+
+## 07
 * 假如條件只判斷一種狀況，且沒有else，那&&可以當作then (22'10)
-  `cartItems.length !== 0 && <div> blabla </div>`
+  ```js
+  cartItems.length !== 0 && <div> blabla </div>
+  ```
 
 * `>`: child combinator (22'51)
   E > F, 以 > 符號分隔2個選擇器,表示會套用在F元素是E元素的子代,如果E跟F中間還有其他的元素就不是子代了。[(ref)](https://powerfuleric.pixnet.net/blog/post/89523013)
-  
+
+## 08 記住上次選項
+* localStorage的使用 和 JSON讀取寫入
+  * localStorage.setItem: 更新 Storage (1'46) [(ref)](https://andy6804tw.github.io/2018/03/04/browser-localstorage/)
+    這裡的更新就等於新增，每次呼叫就等於將資料暫存的內容重新刷新一次。
+    * 將資料存入 Storage:
+      ```js
+      localStorage.setItem('todoList', JSON.stringify(data));
+      ```
+      其中，JSON.stringify(): 將任何物件轉變為 JSON 字串 [(ref)](https://medium.com/itsems-frontend/javascript-json-stringify-and-json-parse-7a1251d3824c)
+    * JSON 變物件: JSON.parse() (3'02)
+      可以接收 JSON 字串，轉為 Javascript 物件或是值。
--- src/components/Cart.js
+++ src/components/Cart.js
@@ -2,6 +2,35 @@ import React, { Component } from "react";
 import formatCurrency from "../util";
 
 export default class Cart extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name:"",
+      email:"",
+      phoneNumber:"",
+      showCheckout: false,
+    };
+  }
+  handleInput = (e) =>{
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  };
+
+  createOrder = (e) =>{
+    e.preventDefault();
+    const order ={
+      name: this.state.name,
+      email: this.state.email,
+      phoneNumber: this.state.phoneNumber,
+      cartItems: this.props.cartItems,
+    };
+    this.props.createOrder(order);
+  };
+
+
+
+
   render() {
     const { cartItems } = this.props;
     return (
@@ -26,7 +55,10 @@ export default class Cart extends Component {
                     <div>{item.title}</div>
                     <div className="right">
                       {formatCurrency(item.price)} x{item.count}{" "}
-                      <button className="button" onClick={() => this.props.removeFromCart(item)}>
+                      <button
+                        className="button"
+                        onClick={() => this.props.removeFromCart(item)}
+                      >
                         Remove
                       </button>
                     </div>
@@ -35,23 +67,68 @@ export default class Cart extends Component {
               ))}
             </ul>
           </div>
-          {cartItems.length !== 0 &&
-            (
+          {cartItems.length !== 0 && (
+            <div>
               <div className="cart">
                 <div className="total">
                   <div>
-                    Total: {" "}
-                    {formatCurrency(cartItems.reduce((a, c)=> a + c.price * c.count, 0))}
+                    Total:{" "}
+                    {formatCurrency(
+                      cartItems.reduce((a, c) => a + c.price * c.count, 0)
+                    )}
                   </div>
-                  <button className="button primary">Proceed</button>
+                  <button
+                    onClick={() => {
+                      this.setState({ showCheckout: true });
+                    }}
+                    className="button primary"
+                  >
+                    Proceed
+                  </button>
                 </div>
               </div>
-            )
+              {this.state.showCheckout && (
+                <div className="cart">
+                  <form onSubmit={this.createOrder}>
+                    <ul className="form-container">
+                      <li>
+                        <label>Email</label>
+                        <input
+                          name="email"
+                          type="email"
+                          required
+                          onChange={this.handleInput}
+                        ></input>
+                      </li>
 
-          }
-          
+                      <li>
+                        <label>Name</label>
+                        <input
+                          name="name"
+                          type="text"
+                          required
+                          onChange={this.handleInput}
+                        ></input>
+                      </li>
 
-
+                      <li>
+                        <label>Phone Number</label>
+                        <input
+                          name="phoneNumber"
+                          type="text"
+                          required
+                          onChange={this.handleInput}
+                        ></input>
+                      </li>
+                      <li>
+                        <button className="button primary" type="submit">Checkout</button>
+                      </li>
+                    </ul>
+                  </form>
+                </div>
+              )}
+            </div>
+          )}
         </div>
       </div>
     );
